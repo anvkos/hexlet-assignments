8e99479dbb995f75187b784fe4b932774da574c8
@hexlet/cli <support@hexlet.io>
2022-11-14 22:28:53 +0300

    submit rails-basics-ru/i18n

---
+++ rails-basics-ru/i18n/config/routes.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+Rails.application.routes.draw do
+  # BEGIN
+  root 'home#index'
+
+  resources :posts do
+    scope module: 'posts' do
+      resources :comments
+    end
+  end
+  # END
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
